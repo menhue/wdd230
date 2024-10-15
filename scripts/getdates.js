c820089a04301fd1bbf9e61003e5c7cf82d2c826
kukajue <kenneleenue40@yahoo.com>
2024-10-15 13:50:55 +0000

    home page

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,3 +5,14 @@ const lastModified = document.lastModified;
 document.getElementById("copyright").textContent = `© ${currentYear} Your Website Name`;
 
 document.getElementById("lastModified").textContent = `Last updated: ${lastModified}`;
+const hamburgerBtn = document.getElementById('hamburger-btn');
+const menu = document.getElementById('menu');
+
+hamburgerBtn.addEventListener('click', function () {
+  menu.classList.toggle('active');
+  if (menu.classList.contains('active')) {
+    hamburgerBtn.innerHTML = 'X';
+  } else {
+    hamburgerBtn.innerHTML = '&#9776;';
+  }
+});
